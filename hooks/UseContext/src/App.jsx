dec9524b11670b1d84324bfd56a23d68faad1f2b
MuriloCalmon <murilocalmon80@gmail.com>
2023-02-04 11:26:32 -0300

    UseContext + Eslint

---
+++ hooks/UseContext/src/App.jsx
@@ -0,0 +1,15 @@
+import { Div } from './components/Div';
+import { AppContext } from './context';
+import './index.css';
+
+
+function App() {
+
+	return (
+		<AppContext>
+			<Div />
+		</AppContext>
+	);
+}
+
+export default App;
