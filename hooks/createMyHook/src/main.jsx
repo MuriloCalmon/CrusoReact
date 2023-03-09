f8f9978002e5b02a1601f83db37dfad98f2debe4
MuriloCalmon <murilocalmon80@gmail.com>
2023-03-08 22:09:24 -0300

    compoundComponents

--- hooks/createMyHook/src/main.jsx
+++ hooks/createMyHook/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
 //import { Error } from './ErrorBoudaryExample'
-//import { CompoundComponents } from '../compoundComponents'
+import { CompoundComponents } from './compoundComponents'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
